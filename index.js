c51ccc043f023e1491672a08505c17f60e938bb6
Gaurav Sharma <113518199+GauravsSharma@users.noreply.github.com>
2023-11-11 13:23:38 +0530

    new changes

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cors = require('cors')
 
 const app = express();
 app.use(cors())
-const  apiData = require('./data.json');
+const  apiData = require('./MOCK_DATA.json');
 const port = process.env.PORT||3000
 app.get('/',(req,res)=>{
     res.send("Hello im live")
 
 })
-app.get("/service",()=>{
-    res.send(apiData);
+app.get("/cara/product",(req , res)=>{
+    return res.json(apiData);
 })
 app.listen(port,()=>{
  console.log("Im live at port",port);
